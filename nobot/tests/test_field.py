5c1733895f5fc7a63990c19b1db6d40bf91f9354
Christopher Grebs <cg@webshox.org>
2015-02-19 14:42:27 +0100

    More client.render tests

--- nobot/tests/test_field.py
+++ nobot/tests/test_field.py
@@ -38,3 +38,37 @@ class TestReCaptchaClient(object):
                 'noscript_url': '//www.google.com/recaptcha/api/noscript?k=pubkey&hl=en',  # noqa
             }
         )
+
+    @mock.patch('django.template.loader.render_to_string')
+    def test_render_has_error(self, render_to_string, activate_en):
+        client = ReCaptchaClient()
+        client.render({}, 'foo bar')
+
+        assert render_to_string.called_once_with(
+            'captcha/widget.html',
+            {
+                'api_server': '//www.google.com/recaptcha/api',
+                'public_key': 'pubkey',
+                'lang': 'en',
+                'options': '{"lang": "en"}',
+                'challenge_url': '//www.google.com/recaptcha/api/challenge?k=pubkey&hl=en&error=foo%20bar',  # noqa
+                'noscript_url': '//www.google.com/recaptcha/api/noscript?k=pubkey&hl=en&error=foo%20bar',  # noqa
+            }
+        )
+
+    @mock.patch('django.template.loader.render_to_string')
+    def test_render_uses_language(self, render_to_string, activate_en):
+        client = ReCaptchaClient()
+        client.render({'lang': 'de'}, 'foo bar')
+
+        assert render_to_string.called_once_with(
+            'captcha/widget.html',
+            {
+                'api_server': '//www.google.com/recaptcha/api',
+                'public_key': 'pubkey',
+                'lang': 'de',
+                'options': '{"lang": "de"}',
+                'challenge_url': '//www.google.com/recaptcha/api/challenge?k=pubkey&hl=de&error=foo%20bar',  # noqa
+                'noscript_url': '//www.google.com/recaptcha/api/noscript?k=pubkey&hl=de&error=foo%20bar',  # noqa
+            }
+        )
